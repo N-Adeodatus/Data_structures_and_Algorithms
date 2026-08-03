b6ec8b0577ccf70a0b40580ce1d9dc46aceb8806
N-Adeodatus <nkundiman3@gmail.com>
2026-08-03 18:10:39 +0200

    add checkout_book function

--- Library_book_checkout/main.c
+++ Library_book_checkout/main.c
@@ -3,6 +3,7 @@
 #define NUMBER_OF_BOOKS 5
 
 void display_books(int books_arr[], int size);
+int checkout_book(int books_arr[], int size, int book_id);
 
 int main(void) {
     int books[NUMBER_OF_BOOKS] = {0};
@@ -14,4 +15,22 @@ void display_books(int books_arr[], int size) {
     for(int i = 0; i < size -1; i++ ) {
         printf("book ID %d is %s", i + 1, books_arr[i] == 0 ? "available" : "not available");
     }
+}
+
+int checkout_book(int books_arr[], int size, int book_id) {
+    int index = book_id - 1;
+
+    if (index < 0 || index > 4) {
+        return -1;
+    }
+    
+    if (index == 1) {
+        books_arr[index] = 0;
+        return 0;
+    }
+
+    books_arr[index] = 1;
+
+    return 1;
+
 }
