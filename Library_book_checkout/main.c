d3536956cf713081e7488b799b090222d14359e2
N-Adeodatus <nkundiman3@gmail.com>
2026-08-03 17:57:42 +0200

    add display_books function

--- Library_book_checkout/main.c
+++ Library_book_checkout/main.c
@@ -2,8 +2,16 @@
 
 #define NUMBER_OF_BOOKS = 5
 
+void display_books(int books_arr[], int size);
+
 int main(void) {
     int books[NUMBER_OF_BOOKS] = {0};
 
     return 0;
+}
+
+void display_books(int books_arr[], int size) {
+    for(int i = 0; i < size -1; i++ ) {
+        printf("book ID %d is %s", i + 1, books[i] == 0 ? "available" : "not available");
+    }
 }
