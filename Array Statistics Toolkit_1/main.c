911634da2a1d21e1614d38082f109722b3a9cc9e
N-Adeodatus <nkundiman3@gmail.com>
2026-08-03 11:04:36 +0200

    create search, min, and max functions

--- Array Statistics Toolkit_1/main.c
+++ Array Statistics Toolkit_1/main.c
@@ -5,12 +5,18 @@
 void print_array(int arr[], int size);
 int sum_array(int arr[], int size);
 float average_array(int arr[], int size);
+int min_array(int arr[], int size);
+int max_array(int arr[], int size);
+int linear_search(int arr[], int size, int target);
 
 int main(void)
 {
     int numbers[ARRAY_SIZE] = {23, 4, 67, 12, 89};
     int total;
     float avg;
+    int smallest;
+    int largest;
+    int index;
 
     print_array(numbers, ARRAY_SIZE);
 
@@ -19,6 +25,22 @@ int main(void)
 
     avg = average_array(numbers, ARRAY_SIZE);
     printf("Average: %.2f\n", avg);
+
+    smallest = min_array(numbers, ARRAY_SIZE);
+    printf("Min: %d\n", smallest);
+
+    largest = max_array(numbers, ARRAY_SIZE);
+    printf("Max: %d\n", largest);
+
+    index = linear_search(numbers, ARRAY_SIZE, 67);
+    if (index != -1)
+    {
+        printf("Found 67 at index %d\n", index);
+    }
+    else
+    {
+        printf("67 not found\n");
+    }
 
     return 0;
 }
@@ -51,4 +73,51 @@ float average_array(int arr[], int size)
 {
     int total = sum_array(arr, size);
     return (float)total / size;
+}
+
+int min_array(int arr[], int size)
+{
+    int i;
+    int smallest = arr[0];
+
+    for (i = 1; i < size; i++)
+    {
+        if (arr[i] < smallest)
+        {
+            smallest = arr[i];
+        }
+    }
+
+    return smallest;
+}
+
+int max_array(int arr[], int size)
+{
+    int i;
+    int largest = arr[0];
+
+    for (i = 1; i < size; i++)
+    {
+        if (arr[i] > largest)
+        {
+            largest = arr[i];
+        }
+    }
+
+    return largest;
+}
+
+int linear_search(int arr[], int size, int target)
+{
+    int i;
+
+    for (i = 0; i < size; i++)
+    {
+        if (arr[i] == target)
+        {
+            return i;
+        }
+    }
+
+    return -1;
 }
