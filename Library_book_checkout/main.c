6ef4a29c377bcf9ea096c7931b883e94710ab188
N-Adeodatus <nkundiman3@gmail.com>
2026-08-03 20:35:04 +0200

    fix bugs

--- Library_book_checkout/main.c
+++ Library_book_checkout/main.c
@@ -12,17 +12,24 @@ int main(void) {
     int book_ID;
 
     do {
+        printf("\nMenu:\n");
+        printf("1. View book IDs\n");
+        printf("2. checkout a book\n");
+        printf("3. Exit\n");
+        printf("Choice: ");
+        scanf("%d", &choice);
         switch (choice){
             case 1:
                 display_books(books, NUMBER_OF_BOOKS);
                 break;
             
             case 2:
-                printf("Please enter the book ID");
+                printf("Please enter the book ID\n");
                 scanf("%d", &book_ID);
                 result = checkout_book(books, NUMBER_OF_BOOKS, book_ID);
                 if (result == 1) {
                     printf("Book with id %d checked out successfully\n", book_ID);
+                    
                 }
                 else if (result == 0) {
                     printf("Book with id %d is already checked out\n", book_ID);
@@ -30,19 +37,25 @@ int main(void) {
                 else {
                     printf("Invalid\n");
                 }
-    
-            default:
+                break;
+
+            case 3:
                 printf("Good bye!\n");
+                break;
+            
+            default:
+                printf("Invalid Input");
+                break;
         }
 
-    } while (choice >= 0 && choice < 2);
+    } while (choice != 3);
 
     return 0;
 }
 
 void display_books(int books_arr[], int size) {
-    for(int i = 0; i < size -1; i++ ) {
-        printf("book ID %d is %s", i + 1, books_arr[i] == 0 ? "available" : "not available");
+    for(int i = 0; i < size; i++ ) {
+        printf("book ID %d is %s\n", i + 1, books_arr[i] == 0 ? "available" : "not available");
     }
 }
 
@@ -53,7 +66,7 @@ int checkout_book(int books_arr[], int size, int book_id) {
         return -1;
     }
     
-    if (index == 0) {
+    if (books_arr[index] == 0) {
         books_arr[index] = 1;
         return 1;
     }
