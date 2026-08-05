c46b2a1f6cd2c78d34a0b033d1b37d6f1c839b18
N-Adeodatus <nkundiman3@gmail.com>
2026-08-05 09:33:55 +0200

    add print board function to validate and see the board state

--- Multi_Dimensional_Arrays/tik_tak_toe/tik_tak_toe.c
+++ Multi_Dimensional_Arrays/tik_tak_toe/tik_tak_toe.c
@@ -4,7 +4,43 @@
 
 char board[BOARD_SIZE][BOARD_SIZE];
 
+/* Function prototypes */
+void init_board(void);
+void print_board(void);
+
 int main(void) {
-    
+    init_board();
+    print_board();
     return 0;
+}
+
+/* Function definitions */
+
+void init_board(void) {
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        for (int j = 0; j < BOARD_SIZE; j++) {
+            board[i][j] = ' ';
+        }
+    }
+}
+
+void print_board(void) {
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        for (int j = 0; j < BOARD_SIZE; j++) {
+            printf(" %c ", board[i][j]);
+            if (j < BOARD_SIZE - 1) {
+                printf("|");
+            }
+        }
+        printf("\n");
+        if (i < BOARD_SIZE - 1) {
+            for (int k = 0; k < BOARD_SIZE; k++) {
+                printf("---");
+                if (k < BOARD_SIZE - 1) {
+                    printf("+");
+                }
+            }
+            printf("\n");
+        }
+    }
 }
