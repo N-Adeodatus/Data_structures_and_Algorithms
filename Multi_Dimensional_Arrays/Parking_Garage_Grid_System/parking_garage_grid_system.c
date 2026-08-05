200cd7a95e2bc390673bd1a380ae397abc5d4f21
N-Adeodatus <nkundiman3@gmail.com>
2026-08-05 11:15:15 +0200

    initialize grid

--- Multi_Dimensional_Arrays/Parking_Garage_Grid_System/parking_garage_grid_system.c
+++ Multi_Dimensional_Arrays/Parking_Garage_Grid_System/parking_garage_grid_system.c
@@ -5,6 +5,20 @@
 
 char garage[LEVELS][SPOTS_PER_LEVEL];
 
+/* Function prototypes */
+void init_garage(void);
+
 int main(void) {
+    init_garage();
     return 0;
+}
+
+/* Function definitions */
+
+void init_garage(void) {
+    for (int i = 0; i < LEVELS; i++) {
+        for (int j = 0; j < SPOTS_PER_LEVEL; j++) {
+            garage[i][j] = '.';
+        }
+    }
 }
