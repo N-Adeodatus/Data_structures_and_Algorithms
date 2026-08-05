1289fbdec668551237b6f6b5e220cd020190962f
N-Adeodatus <nkundiman3@gmail.com>
2026-08-05 09:58:47 +0200

    add function to place a move

--- Multi_Dimensional_Arrays/tik_tak_toe/tik_tak_toe.c
+++ Multi_Dimensional_Arrays/tik_tak_toe/tik_tak_toe.c
@@ -8,6 +8,7 @@ char board[BOARD_SIZE][BOARD_SIZE];
 void init_board(void);
 void print_board(void);
 void get_move(int *row, int *col);
+int place_move(int row, int col, char symbol);
 
 int main(void) {
     init_board();
@@ -15,7 +16,14 @@ int main(void) {
 
     int row, col;
     get_move(&row, &col);
-    printf("You entered row %d, col %d\n", row, col);
+
+    if (place_move(row, col, 'X')) {
+        printf("Move placed successfully.\n");
+    } else {
+        printf("Invalid move.\n");
+    }
+
+    print_board();
 
     return 0;
 }
@@ -54,4 +62,15 @@ void print_board(void) {
 void get_move(int *row, int *col) {
     printf("Enter row and column (0-2 0-2): ");
     scanf("%d %d", row, col);
+}
+
+int place_move(int row, int col, char symbol) {
+    if (row < 0 || row >= BOARD_SIZE || col < 0 || col >= BOARD_SIZE) {
+        return 0;
+    }
+    if (board[row][col] != ' ') {
+        return 0;
+    }
+    board[row][col] = symbol;
+    return 1;
 }
