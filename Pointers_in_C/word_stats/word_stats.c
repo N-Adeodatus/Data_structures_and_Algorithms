ee4e0b3d2243687828ac25c6d76c9d5b40b133c9
N-Adeodatus <nkundiman3@gmail.com>
2026-08-06 17:39:24 +0200

    define the data

--- Pointers_in_C/word_stats/word_stats.c
+++ Pointers_in_C/word_stats/word_stats.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
 
 int main(void) {
+    char sentence1[30] = "mango is a delicious fruit";
+    char sentence2[30] = "A computer is a useful tool";
+    char sentence3[30] = "Wild animals live in the forest";
+    char sentence4[30] = "Happy new year!"
+    char sentence5[30] = "congratulations on your promotion."
+
     return 0
 }
