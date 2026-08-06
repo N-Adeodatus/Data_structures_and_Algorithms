499bb0aed9f526d45235d76f021e0a51d1c86ddc
N-Adeodatus <nkundiman3@gmail.com>
2026-08-06 10:13:24 +0200

    add function to calculate length of a string and one to swap variables

--- Pointers_in_C/pointer_operations/pointer_operations.c
+++ Pointers_in_C/pointer_operations/pointer_operations.c
@@ -1,7 +1,40 @@
 #include <stdio.h>
 
+void swap(int *a, int *b);
+int my_strlen(const char *s);
+
 int main(void)
 {
     printf("Pointer Toolkit initialized.\n");
+
+    int x = 5;
+    int y = 10;
+
+    printf("Before swap: x = %d, y = %d\n", x, y);
+    swap(&x, &y);
+    printf("After swap:  x = %d, y = %d\n", x, y);
+
+    const char *word = "pointers";
+    printf("Length of \"%s\" is %d\n", word, my_strlen(word));
+
     return (0);
+}
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int my_strlen(const char *s)
+{
+    int len = 0;
+
+    while (*s != '\0')
+    {
+        len++;
+        s++;
+    }
+    return (len);
 }
