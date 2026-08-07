b1ca1e30ad642c840e1fe0884ef7ce0ad4aceaa2
N-Adeodatus <nkundiman3@gmail.com>
2026-08-07 19:23:55 +0200

    add total_inventory_value function

--- Structures_in_C/Book_Inventory_Tracker/book_inventory_tracker.c
+++ Structures_in_C/Book_Inventory_Tracker/book_inventory_tracker.c
@@ -8,14 +8,18 @@ struct book {
 };
 
 void print_books();
+float total_inventory_value(struct list_of_books[], size);
 
 int main(void) {
+    float sum;
     struct book books_list[3] = {
         {"Essentialism", "Greg", 3.7},
         {"Atomic Habit", "James", 5.2},
         {"Art of War", "Sun Tzu", 7.0}
     };
-    print_books(books_list, 3);
+    print_books(books_list, , LENGTH);
+    sum = total_inventory_value(books_list, LENGTH);
+    printf("The sum of all books' prices is : %.2f", sum);
     return 0;
 }
 
@@ -24,4 +28,12 @@ void print_books(struct book list_of_books[], int length) {
         printf("Book Name: %s\nAuthor: %s\nPrice: %.1f", list_of_books[i].title, list_of_books[i].author, list_of_books[i].price);
         printf("\n---------------------------------\n");
     }
+}
+
+float total_inventory_value(struct list_of_books[], size) {
+    float total = 0;
+    for(int i = 0; i < length; i++) {
+        total = total + list_of_books[i].price;
+    }
+    return total
 }
