ce2d66beb354ede78cb351414efe0e94008daa2e
N-Adeodatus <nkundiman3@gmail.com>
2026-08-07 18:57:10 +0200

    implement print_books function

--- Structures_in_C/Book_Inventory_Tracker/book_inventory_tracker.c
+++ Structures_in_C/Book_Inventory_Tracker/book_inventory_tracker.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#define LENGTH 5
 
 struct book {
     char title[100];
@@ -6,17 +7,20 @@ struct book {
     float price;
 }
 
-void print_book(void);
+void print_books(void);
 
 int main(void) {
-    struct book books_list[5] = {
+    struct book books_list[3] = {
         struct book b1 = {"Essentialism", "Greg", 3.7};
         struct book b2 = {"Atomic Habit", "James", 5.2};
         struct book b3 = {"Art of War", "Sun Tzu", 7.0};
     } 
+    print_books(books_list, 3);
     return 0;
 }
 
-void print_book(void) {
-    
+void print_books(struct book list_of_books[], int length) {
+    for(int i = 0; i < length; i++) {
+        printf("Book Name: %s\nAuthor: %s\nPrice: %f", list_of_books[i].title list_of_books[i].author list_of_books[i].price);
+    }
 }
