acff5e619e264c41b2b85a3607491344ee8567af
N-Adeodatus <nkundiman3@gmail.com>
2026-08-07 19:15:46 +0200

    fix bugs

--- Structures_in_C/Book_Inventory_Tracker/book_inventory_tracker.c
+++ Structures_in_C/Book_Inventory_Tracker/book_inventory_tracker.c
@@ -5,22 +5,23 @@ struct book {
     char title[100];
     char author[50];
     float price;
-}
+};
 
-void print_books(void);
+void print_books();
 
 int main(void) {
     struct book books_list[3] = {
-        struct book b1 = {"Essentialism", "Greg", 3.7};
-        struct book b2 = {"Atomic Habit", "James", 5.2};
-        struct book b3 = {"Art of War", "Sun Tzu", 7.0};
-    } 
+        {"Essentialism", "Greg", 3.7},
+        {"Atomic Habit", "James", 5.2},
+        {"Art of War", "Sun Tzu", 7.0}
+    };
     print_books(books_list, 3);
     return 0;
 }
 
 void print_books(struct book list_of_books[], int length) {
     for(int i = 0; i < length; i++) {
-        printf("Book Name: %s\nAuthor: %s\nPrice: %f", list_of_books[i].title list_of_books[i].author list_of_books[i].price);
+        printf("Book Name: %s\nAuthor: %s\nPrice: %.1f", list_of_books[i].title, list_of_books[i].author, list_of_books[i].price);
+        printf("\n---------------------------------\n");
     }
 }
