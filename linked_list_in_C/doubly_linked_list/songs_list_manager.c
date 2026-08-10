afa9bf280d25c1289a3a151e8abf58eef1cadd7e
N-Adeodatus <nkundiman3@gmail.com>
2026-08-10 16:36:05 +0200

    create struct

---
+++ linked_list_in_C/doubly_linked_list/songs_list_manager.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* ----- Data structure ----- */
+
+typedef struct song_s
+{
+    char *title;
+    int duration;
+    struct song_s *next;
+    struct song_s *prev;
+} song_t;
+
+/* ----- Function prototypes ----- */
+
+/* (we'll add these as we build each function) */
+
+int main(void)
+{
+    return (0);
+}
+
+/* ----- Function definitions ----- */
+
+/* (we'll add these as we build each function) */
