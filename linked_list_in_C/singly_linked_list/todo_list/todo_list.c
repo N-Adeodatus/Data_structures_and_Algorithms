e1037da1ae0b014901535f0883f0a5fcaed4d6d9
N-Adeodatus <nkundiman3@gmail.com>
2026-08-12 20:34:28 +0200

    fix bugs

--- linked_list_in_C/singly_linked_list/todo_list/todo_list.c
+++ linked_list_in_C/singly_linked_list/todo_list/todo_list.c
@@ -16,12 +16,26 @@ typedef struct todo_list{
 
 void init(list *list_of_tasks);
 task *create_node(char *descript);
-void add_to_front(task **head);
+void add_to_front(task **head, task *task_to_add);
 void display_tasks(task *head);
-void remove_task_by_name(task **head, char *name);
+void remove_task_by_description(task **head, char *name);
 
 int main(void) {
+    list students_list;
 
+    init(&students_list);
+
+    task *t1 = create_node("task 1");
+    task *t2 = create_node("task 2");
+
+    add_to_front(&(students_list.head), t1);
+    add_to_front(&(students_list.head), t2);
+
+    display_tasks(students_list.head);
+
+    remove_task_by_description(&(students_list.head), "task 1");
+    
+    display_tasks(students_list.head);
     return 0;
 }
 
@@ -30,9 +44,12 @@ void init(list *list_of_tasks) {
 }
 
 task *create_node(char *descript) {
-    task *new_node = (task) malloc(sizeof(task));
-    strncpy(new_node->task_description, descript, sizeof(new_node->task));
-    new_node->task_description[sizeof(new_node->task) - 1] = '\0';
+    task *new_node = (task*) malloc(sizeof(task));
+    if(new_node == NULL) {
+        return NULL;
+    }
+    strncpy(new_node->task_description, descript, sizeof(new_node->task_description));
+    new_node->task_description[sizeof(new_node->task_description) - 1] = '\0';
     new_node->is_complete = 0;
     new_node->next = NULL;
 
@@ -47,24 +64,26 @@ void add_to_front(task **head, task *task_to_add) {
 void display_tasks(task *head) {
     task *current = head;
     if(head == NULL) {
-        print("The list is empty\n");
+        printf("The list is empty\n");
         return;
     }
+    printf("________List of Tasks________\n");
 
     while(current != NULL) {
-        print("________List of Tasks________\n");
         printf("Description: %s\n", current->task_description);
         printf("Status: %s\n", current->is_complete ? "Complete" : "Incomplete");
-
+        printf("\n");
+        
         current = current->next;
     }
+    printf("--------------------------------------\n");
 }
 
-void remove_task_by_name(task **head, char *name) {
+void remove_task_by_description(task **head, char *name) {
     task *current = *head;
     task *prev = NULL;
     if(head == NULL) {
-        print("The list is empty\n");
+        printf("The list is empty\n");
         return;
     }
 
@@ -77,8 +96,12 @@ void remove_task_by_name(task **head, char *name) {
                 prev->next = current->next;
             }
             free(current);
+            printf("task removed successfully\n");
+            return;
         }
-        current = current->next;
-        prev = current;
+        else {
+            prev = current;
+            current = current->next;
+        }
     }
 }
