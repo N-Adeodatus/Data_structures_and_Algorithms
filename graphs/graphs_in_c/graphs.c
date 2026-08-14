49bddb978bb8157b872720b980e8eb3343badc70
N-Adeodatus <nkundiman3@gmail.com>
2026-08-14 20:51:31 +0200

    add structure

---
+++ graphs/graphs_in_c/graphs.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define INF 1000000
+#define NO_PARENT (-1)
+
+typedef struct edge_s
+{
+    int src;
+    int dest;
+    int weight;
+} edge_t;
+
+/* function prototypes will go here */
+
+int main(void)
+{
+    return (0);
+}
+
+/* function definitions will go here */
