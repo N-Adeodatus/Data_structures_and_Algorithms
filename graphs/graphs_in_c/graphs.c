83f0a189a7c9363528843ac676b945153adfd13b
N-Adeodatus <nkundiman3@gmail.com>
2026-08-14 21:10:32 +0200

    add init function

--- graphs/graphs_in_c/graphs.c
+++ graphs/graphs_in_c/graphs.c
@@ -2,6 +2,7 @@
 
 #define INF 1000000
 #define NO_PARENT (-1)
+#define NUM_VERTICES 5
 
 typedef struct edge_s
 {
@@ -10,11 +11,38 @@ typedef struct edge_s
     int weight;
 } edge_t;
 
-/* function prototypes will go here */
+void init_arrays(int dist[], int parent[], int source);
 
 int main(void)
 {
+    edge_t edges[] = {
+        {0, 1, 6},
+        {0, 2, 7},
+        {1, 2, 8},
+        {1, 3, 5},
+        {1, 4, -4},
+        {2, 3, -3},
+        {2, 4, 9},
+        {3, 1, -2}
+    };
+    int num_edges = sizeof(edges) / sizeof(edges[0]);
+    int dist[NUM_VERTICES];
+    int parent[NUM_VERTICES];
+    int source = 0;
+
+    init_arrays(dist, parent, source);
+
     return (0);
 }
 
-/* function definitions will go here */
+void init_arrays(int dist[], int parent[], int source)
+{
+    int i;
+
+    for (i = 0; i < NUM_VERTICES; i++)
+    {
+        dist[i] = INF;
+        parent[i] = NO_PARENT;
+    }
+    dist[source] = 0;
+}
