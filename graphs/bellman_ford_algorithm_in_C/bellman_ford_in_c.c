48ec83082899501e18f06981e27204e9578d075c
N-Adeodatus <nkundiman3@gmail.com>
2026-08-15 07:55:35 +0200

    add bellman_ford_in_c

---
+++ graphs/bellman_ford_algorithm_in_C/bellman_ford_in_c.c
@@ -0,0 +1,62 @@
+
+#include<stdio.h>
+#define VER 4
+#define EDG 5
+
+typedef struct edge{
+    int source;
+    int destination;
+    int weight;
+} edge;
+
+/*
+Input:
+    - hardcoded edges
+    - number of vertexes.
+    - number of edges
+Storage:
+    - structs for storing edges
+    - array to store the edges.
+    - array to store the shortest distance to each vertex
+Processing:
+    - Have an outer loop to pass through the edges (V - 1) times
+    - Have an inner loop for interate over all the edges
+        - check if a distance to destination needs change
+            - update the distance
+            - update the parent for the path
+Output:
+    - iterate over the dist array:
+        - print distance from A to each vertex
+    
+*/
+
+void bellman_ford(edge edges_arr[], int distances[]);
+int main(void) {
+    edge edges[] = {
+        {0, 1, 4},   // A -> B
+        {0, 2, 2},   // A -> C
+        {1, 3, 3},   // B -> D
+        {2, 3, 1},   // C -> D
+        {1, 2, 1}    // B -> C
+    };
+
+    int dist[] = {0, 999, 999, 999};
+    bellman_ford(edges, dist);
+    printf("__________Distances_________\n");
+    for(int i = 0; i < VER; i++) {
+        printf("A -> %c = %d\n", 'A' + i, dist[i]);
+    }
+
+    return 0;
+}
+
+void bellman_ford(edge edges_arr[], int distances[]){
+    for(int i = 1; i < VER; i++) {
+        for(int j = 0; j < EDG; j++) {
+             if(distances[edges_arr[j].source] + edges_arr[j].weight < distances[edges_arr[j].destination]) {
+                distances[edges_arr[j].destination] = distances[edges_arr[j].source] + edges_arr[j].weight;
+             }
+        }
+    }
+}
+
