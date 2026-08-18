660a5e1008268ad9bb21269f071f261e3b5f7d67
N-Adeodatus <nkundiman3@gmail.com>
2026-08-18 11:42:50 +0200

    create tree

--- Trees/Binary_Trees/binary_trees.c
+++ Trees/Binary_Trees/binary_trees.c
@@ -23,6 +23,14 @@ void free_tree(node_t *root);
 
 int main(void)
 {
+    node_t *root = create_node(1);
+
+    root->left = create_node(2);
+    root->right = create_node(3);
+    root->left->left = create_node(4);
+    root->left->right = create_node(5);
+    root->right->right = create_node(6);
+
     return (0);
 }
 
