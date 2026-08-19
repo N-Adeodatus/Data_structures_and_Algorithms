60117a05d1020cf6fa024336f26bceaf5e388f28
N-Adeodatus <nkundiman3@gmail.com>
2026-08-19 11:55:16 +0200

    add new_node function

--- Trees/AVL_Trees/Minimal_AVL_Tree_Demo/avl_tree_demo.c
+++ Trees/AVL_Trees/Minimal_AVL_Tree_Demo/avl_tree_demo.c
@@ -15,3 +15,32 @@ struct node
     struct node *left;
     struct node *right;
 };
+
+/* Function prototypes */
+struct node *new_node(int data);
+
+int main(void)
+{
+    return (0);
+}
+
+/**
+ * new_node - allocate and initialize a new AVL node
+ * @data: value to store
+ *
+ * Return: pointer to the new node, or NULL on allocation failure
+ */
+struct node *new_node(int data)
+{
+    struct node *n = malloc(sizeof(struct node));
+
+    if (n == NULL)
+        return (NULL);
+
+    n->data = data;
+    n->left = NULL;
+    n->right = NULL;
+    n->height = 1;
+
+    return (n);
+}
