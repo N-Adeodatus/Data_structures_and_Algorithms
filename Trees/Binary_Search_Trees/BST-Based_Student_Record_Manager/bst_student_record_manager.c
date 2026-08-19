6ecc6f9737d08d6358138d4b6c4baa0cdb516049
N-Adeodatus <nkundiman3@gmail.com>
2026-08-19 10:12:09 +0200

    add new_node function

--- Trees/Binary_Search_Trees/BST-Based_Student_Record_Manager/bst_student_record_manager.c
+++ Trees/Binary_Search_Trees/BST-Based_Student_Record_Manager/bst_student_record_manager.c
@@ -15,3 +15,33 @@ struct node
     struct node *left;
     struct node *right;
 };
+
+/* Function prototypes */
+struct node *new_node(int id, float grade);
+
+int main(void)
+{
+    return (0);
+}
+
+/**
+ * new_node - allocate and initialize a new BST node
+ * @id: student ID to store
+ * @grade: student grade to store
+ *
+ * Return: pointer to the new node, or NULL on allocation failure
+ */
+struct node *new_node(int id, float grade)
+{
+    struct node *n = malloc(sizeof(struct node));
+
+    if (n == NULL)
+        return (NULL);
+
+    n->id = id;
+    n->grade = grade;
+    n->left = NULL;
+    n->right = NULL;
+
+    return (n);
+}
